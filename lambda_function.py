7da5f098b89a894b28f052134ea0048b48ea5b27
sharatvishwambharan <91662092+sharatvishwambharan@users.noreply.github.com>
2021-10-22 13:30:53 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,12 @@
 import json
 import boto3
+import urllib.parse
+from urllib.parse import urlparse
+from urllib.parse import unquote_plus
 
 def lambda_handler(event, context):
     # TODO implement
+key_name = unquote_plus(event['Records'][0]['s3']['object']['key'])
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
