9c30dd83f659ca72cb57f72d204cca717888bc20
sharatvishwambharan <91662092+sharatvishwambharan@users.noreply.github.com>
2021-10-22 16:29:16 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,9 +4,17 @@ import urllib.parse
 from urllib.parse import urlparse
 from urllib.parse import unquote_plus
 
-def lambda_handler(event, context):
+def handler(event, context):
     # TODO implement
 key_name = unquote_plus(event['Records'][0]['s3']['object']['key'])
+S3: {
+ "bucket": {
+   "name": "deployment-of-aws-code"
+  },
+  "object": {
+    "key": "01-test-lambda.zip/test-lambda"
+  }
+}
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
