b50349fc8c87ca53e18c30aee5b119331db8c1f8
sharatvishwambharan <91662092+sharatvishwambharan@users.noreply.github.com>
2021-10-28 16:42:42 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,7 @@ import urllib.parse
 from urllib.parse import urlparse
 from urllib.parse import unquote_plus
 
+s3 = boto3.client('s3')
 def handler(event, context):
     # TODO implement
 key_name = unquote_plus(event['Records'][0]['s3']['object']['key'])
