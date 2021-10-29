870b20dd1e37658fcacf8c9ae597dfafaba02783
sharatvishwambharan <91662092+sharatvishwambharan@users.noreply.github.com>
2021-10-29 10:24:03 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,8 @@ import urllib.parse
 from urllib.parse import urlparse
 from urllib.parse import unquote_plus
 
+s3 = boto3.client('s3')
+s3.upload_file('lambda_function.py','deployment-of-aws-code','out.zip')
 def handler(event, context):
     # TODO implement
 key_name = unquote_plus(event['Records'][0]['s3']['object']['key'])
