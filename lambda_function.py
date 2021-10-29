001ba3637e7fc03a7eac8e02c635887058e62ad4
sharatvishwambharan <91662092+sharatvishwambharan@users.noreply.github.com>
2021-10-29 10:09:58 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,6 @@ import urllib.parse
 from urllib.parse import urlparse
 from urllib.parse import unquote_plus
 
-s3 = boto3.client('s3')
 def handler(event, context):
     # TODO implement
 key_name = unquote_plus(event['Records'][0]['s3']['object']['key'])
@@ -16,8 +15,7 @@ S3: {
     "key": "out.zip"
   }     
  #my-first-test
- #my-second-test
- #this is just for test purpose
+ #this is for test purpose
 }
     return {
         'statusCode': 200,
